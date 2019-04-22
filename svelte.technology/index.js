ca8bed2543af7fd44896826ba9aff0fa35ee36a5
Conduitry <git@chor.date>
2019-04-22 10:04:36 -0400

    fix svelte.technology/guide redirect

--- svelte.technology/index.js
+++ svelte.technology/index.js
@@ -3,9 +3,7 @@ const http = require('http');
 const dest = `https://svelte.dev`;
 
 const server = http.createServer((req, res) => {
-	const url = req.url.startsWith('guide')
-		? 'docs'
-		: req.url;
+	const url = req.url === '/guide' ? '/docs' : req.url;
 
 	res.writeHead(301, {
 		'Location': `${dest}${url}`
